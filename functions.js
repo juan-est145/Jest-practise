ef507b495869f148857f4d8591387133fc466f76
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2024-06-25 17:56:36 +0200

    First tests are done

--- functions.js
+++ functions.js
@@ -1,1 +1,5 @@
-const capitalize = require("./functions")
+exports.capitalize = function (string) {
+	if (string === undefined || string.length === 0 || typeof string !== "string")
+		return (string);
+	return (string[0].toUpperCase() + string.slice(1));
+}
--- functions.test.js
+++ functions.test.js
@@ -1,1 +1,17 @@
-exports.Capitalize = this.Capitalize;
+const { capitalize } = require("./functions");
+
+test("Capitalize simple case", () => {
+	expect(capitalize("hola")).toBe("Hola");
+});
+
+test("Capitalize numbers", () => {
+	expect(capitalize("123")).toBe("123");
+});
+
+test("Capitalize empty", () => {
+	expect(capitalize("")).toBe("");
+});
+
+test("Capitalize non string", () => {
+	expect(capitalize(42)).toBe(42);
+});
