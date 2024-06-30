f581028ac8afa1a74bdc4cb8d619a5a7a0f1a1ba
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2024-06-30 10:44:13 +0200

    Changed syntax of capitalize test

--- functions.js
+++ functions.js
@@ -1,5 +1,7 @@
-exports.capitalize = function (string) {
+function capitalize (string) {
 	if (string === undefined || string.length === 0 || typeof string !== "string")
 		return (string);
 	return (string[0].toUpperCase() + string.slice(1));
 }
+
+module.exports = {capitalize};
